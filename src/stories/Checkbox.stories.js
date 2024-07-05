40ac0d50e52d4f297a1ee6ee306af6a143fcbdb8
이수민 <sooper1137@gmail.com>
2024-07-05 19:13:21 +0900

    체크박스 스타일 스토리북 업데이트

--- src/stories/Checkbox.stories.js
+++ src/stories/Checkbox.stories.js
@@ -39,6 +39,14 @@ const CheckboxContainer = styled.label`
       &::after {
         background-color: ${(props) => props.theme.primaryColor};
         border-color: ${(props) => props.theme.checkedgrayColor};
+        background-image: url("data:image/svg+xml,%3Csvg viewBox='0 -0.5 17 17' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' class='si-glyph si-glyph-checked' fill='${encodeURIComponent(
+          props.fillColor || "#ffffff"
+        )}'%3E%3Cg id='SVGRepo_bgCarrier' stroke-width='0'%3E%3C/g%3E%3Cg id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'%3E%3C/g%3E%3Cg id='SVGRepo_iconCarrier'%3E%3Ctitle%3E1228%3C/title%3E%3Cdefs%3E%3C/defs%3E%3Cg stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'%3E%3Cpath d='M3.432,6.189 C3.824,5.798 4.455,5.798 4.847,6.189 L6.968,8.31 L13.147,2.131 C13.531,1.747 14.157,1.753 14.548,2.144 L16.67,4.266 C17.06,4.657 17.066,5.284 16.684,5.666 L7.662,14.687 C7.278,15.07 6.651,15.064 6.261,14.673 L1.311,9.723 C0.92,9.333 0.92,8.7 1.311,8.31 L3.432,6.189 Z' fill='${encodeURIComponent(
+          props.fillColor || "#ffffff"
+        )}' class='si-glyph-fill'%3E%3C/path%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+        background-repeat: no-repeat;
+        background-size: 80%;
+        background-position: center center;
       }
     `}
 `;
@@ -123,17 +131,6 @@ UnChecked.args = {
   theme: "light",
 };
 
-export const DarkTheme = (args) => (
-  <ThemeProvider theme={args.theme === "dark" ? darkTheme : lightTheme}>
-    <Checkbox {...args} />
-  </ThemeProvider>
-);
-DarkTheme.args = {
-  label: "",
-  checked: false,
-  onChange: () => {},
-  theme: "dark",
-};
 
 export const DarkThemeChecked = (args) => (
   <ThemeProvider theme={args.theme === "dark" ? darkTheme : lightTheme}>
@@ -143,6 +140,18 @@ export const DarkThemeChecked = (args) => (
 DarkThemeChecked.args = {
   label: "",
   checked: true,
+  onChange: () => {},
+  theme: "dark",
+};
+
+export const DarkTheme = (args) => (
+  <ThemeProvider theme={args.theme === "dark" ? darkTheme : lightTheme}>
+    <Checkbox {...args} />
+  </ThemeProvider>
+);
+DarkTheme.args = {
+  label: "",
+  checked: false,
   onChange: () => {},
   theme: "dark",
 };
